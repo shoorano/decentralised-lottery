f6e1a980f2549213d0aee447785783b66641d9c4
Shoorano <danwilloughby2@gmail.com>
2021-11-04 18:30:03 +0000

    added basic skeleton to Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,5 +3,24 @@
 pragma solidity ^0.6.6;
 
 contract Lottery {
-    
+    address payable[] public players;
+    uint256 public USDEntryFee;
+    // v3aggregator blah = priceFeed
+    constructor(address priceFeed) public {
+        // pass priceFeed to v3 aggregator
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        require(msg.value > getEntranceFee(), "Sent amount is not enough");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return 123;
+    }
+
+    function startLottery() public {}
+
+    function endLottery() public {}
 }
