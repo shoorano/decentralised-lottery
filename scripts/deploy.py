b9cb44484e41fc54fbb535dcadcc71e69be8c807
Shoorano <danwilloughby2@gmail.com>
2021-11-10 13:35:47 +0000

    finished lottery deploy.py script, added link funding and automodated contract fetching for constructor args

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,12 +1,59 @@
+from time import sleep
 from brownie import Lottery, config, network
-from scripts.helpers import DECIMALS, STARTING_PRICE, get_account, get_eth_price_feed_address
+from scripts.helpers import *
+
 
 def deploy_lottery():
-    """basic deployer method"""
+    """grabs network based addresses and contract constructor args and returns lottery
+    contract, deploys contract if contract object is empty"""
     account = get_account()
-    price_feed_address = get_eth_price_feed_address()
-    return Lottery.deploy(price_feed_address, {"from": account}, publish_source=config["networks"][network.show_active()].get("verify"))
+    if len(Lottery) > 0:
+        return Lottery[-1]
+    return Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("link_token").address,
+        get_contract("vrf_coordinator").address,
+        get_brownie_config_variable("vrf_coordinator_key_hash"),
+        get_brownie_config_variable("vrf_coordinator_fee"),
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False)
+    )
+
+def start_lottery():
+    """starts lottery"""
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_txn = lottery.startLottery({"from": account})
+    starting_txn.wait(1)
+    print("The lottery has started")
+
+def add_participants():
+    """"""
+    account = get_account()
+    lottery = Lottery[-1]
+    entrance_fee = lottery.getEntranceFee({"from": account})
+    for i in range(1, 5):
+        account = get_account(index=i)
+        enter_txn = lottery.enter({"from": account, "value": entrance_fee*1.2})
+        enter_txn.wait(1)
+    print("Participants have entered the lottery")
+
+def end_lottery():
+    """ends lottery"""
+    account = get_account()
+    lottery = Lottery[-1]
+    fund_with_link(lottery.address, amount=get_brownie_config_variable("vrf_coordinator_fee")*20)
+    ending_txn = lottery.endLottery({"from": account})
+    ending_txn.wait(1)
+    sleep(60)
+    print("The lottery has ended")
+    print(f"The winner was: {lottery.lastWinner}")
+    
+
 
 def main():
     """runs deployment method"""
     deploy_lottery()
+    start_lottery()
+    add_participants()
+    end_lottery()
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,4 +1,4 @@
-from brownie import MockV3Aggregator, accounts, config, network
+from brownie import MockV3Aggregator, VRFCoordinatorMock, LinkToken, interface, accounts, config, network, Contract
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "hardhat-local"]
@@ -6,32 +6,79 @@ HARDHAT_CHAIN_ID=31337
 HARDHAT_HOST="http://127.0.0.1:8545/"
 DECIMALS=8
 UNIT="ether"
-STARTING_PRICE=4477.85*10**8
+INITIAL_AMOUNT=4477.85*10**8
 
-def get_account():
-    """return account based on chain"""
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
+def get_account(index=None, id=None):
+    """return account based on chain:
+    - index identifies a different account from local / mainnet-fork accounts
+    - id identifies accounts stored within brownie for testnet and mainnet usage
+    """
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
-    accounts.add(config["wallets"]["from_key"])
-    return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
 
-def get_eth_price_feed_address():
-    """returns price feed address based on chain, deploys mock if
+def get_brownie_config_variable(variable):
+    """returns brownie config variable based on network running, deploys mock if
     on local chain"""
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
-        return config["networks"][network.show_active()]["eth_usd_price_feed_address"]
-    else:
-        return deploy_mock_v3_aggregator()
+    return config["networks"][network.show_active()][variable]
+    
 
-def deploy_mock_v3_aggregator():
+def get_contract(contract_name):
+    """This function will grab the contract addresses from the brownie config
+    if defined, otherwise, it will deploy a mock version of that contract, and
+    return that mock contract.
+    
+        Args:
+            contract_name (string)
+        
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            version of this contract.
+    """
+    contract_to_mock = {
+        "eth_usd_price_feed": MockV3Aggregator,
+        "vrf_coordinator": VRFCoordinatorMock,
+        "link_token": LinkToken
+    }
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        return contract_type[-1]
+    contract_address = config["networks"][network.show_active()][contract_name]
+    return Contract.from_abi(
+        contract_type._name,
+        contract_address,
+        contract_type.abi
+    )
+
+def deploy_mocks(decimals=DECIMALS, initial_amount=INITIAL_AMOUNT):
     """deploys MockV3Aggregator contract"""
     print(f"Active network is {network.show_active()}")
-    if len(MockV3Aggregator) > 0:
-        return MockV3Aggregator[-1].address
-    print("Deploying MockV3Aggregator")
-    mock_v3_aggregator = MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
-    print("Deployed MockV3Aggregator")
-    return mock_v3_aggregator.address
+    print("Deploying mocks")
+    account = get_account()
+    MockV3Aggregator.deploy(decimals, initial_amount, {"from": account})
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Deployed mocks")
+
+def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000): # 0.1 link
+    """funds address with link token"""
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    # Can be used this way if contract does not give access to all methods
+    # Also skips the step of grabbing abi and compiling as brownie automates this
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    tx.wait(1)
+    print("Funded contract with Link")
 
 def to_wei(amount, unit):
     conversion_dictionary = {
